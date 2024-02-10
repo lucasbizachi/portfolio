efc8a0fd8047bd6cfd99ed3993e0ecfb6281fa6f
lucasbizachi <bizachilucas@gmail.com>
2024-02-10 13:25:46 -0300

    api url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/lucasbizachi/portfolio/main/data/profile.json';
+    const url = 'https://avatars.githubusercontent.com/u/101759223?v=4';
     const fetching = await fetch(url)
     return await fetching.json()
 
