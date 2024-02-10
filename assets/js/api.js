b16d0017e44953a0d582cf88eafd6a089b60b054
lucasbizachi <bizachilucas@gmail.com>
2024-02-10 13:21:24 -0300

    adicionando informações profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/lucasbizachi/portfolio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 
